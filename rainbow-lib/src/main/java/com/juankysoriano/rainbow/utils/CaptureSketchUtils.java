19e61ba49a824745320e9ebde6757ed5352b8071
Juanky Soriano <juanky@juankysoriano.com>
2015-04-21 01:31:39 +0100

    Revert "Removes unneeded class"
    This reverts commit 2feceeda68036398de85504c52bed27fa3d67a82.

---
+++ rainbow-lib/src/main/java/com/juankysoriano/rainbow/utils/CaptureSketchUtils.java
@@ -0,0 +1,123 @@
+package com.juankysoriano.rainbow.utils;
+
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.ContentValues;
+import android.graphics.Bitmap;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.provider.MediaStore.Images;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Android internals have been modified to store images in the media folder with
+ * the correct date meta data
+ *
+ * @author samuelkirton
+ */
+public abstract class CaptureSketchUtils {
+
+    /**
+     * A copy of the Android internals  insertImage method, this method populates the
+     * meta data with DATE_ADDED and DATE_TAKEN. This fixes a common problem where media
+     * that is inserted manually gets saved at the end of the gallery (because date is not populated).
+     *
+     * @see android.provider.MediaStore.Images.Media#insertImage(ContentResolver, Bitmap, String, String)
+     */
+    public static Uri insertImage(ContentResolver cr,
+                                           Bitmap source,
+                                           String title,
+                                           String description) {
+
+        ContentValues values = new ContentValues();
+        values.put(Images.Media.TITLE, title);
+        values.put(Images.Media.DISPLAY_NAME, title);
+        values.put(Images.Media.DESCRIPTION, description);
+        values.put(Images.Media.MIME_TYPE, "image/jpeg");
+        values.put(Images.Media.DATE_ADDED, System.currentTimeMillis());
+        values.put(Images.Media.DATE_TAKEN, System.currentTimeMillis());
+
+        Uri uri = null;
+
+        try {
+            uri = cr.insert(Images.Media.INTERNAL_CONTENT_URI, values);
+
+            if (source != null) {
+                OutputStream imageOut = cr.openOutputStream(uri);
+                try {
+                    source.compress(Bitmap.CompressFormat.JPEG, 100, imageOut);
+                } finally {
+                    imageOut.close();
+                }
+
+                long id = ContentUris.parseId(uri);
+                // Wait until MINI_KIND thumbnail is generated.
+                Bitmap miniThumb = Images.Thumbnails.getThumbnail(cr, id, Images.Thumbnails.MINI_KIND, null);
+                // This is for backward compatibility.
+                storeThumbnail(cr, miniThumb, id, 50F, 50F, Images.Thumbnails.MICRO_KIND);
+            } else {
+                cr.delete(uri, null, null);
+                uri = null;
+            }
+        } catch (Exception e) {
+            if (uri != null) {
+                cr.delete(uri, null, null);
+                uri = null;
+            }
+        }
+
+        return uri;
+    }
+
+    /**
+     * A copy of the Android internals StoreThumbnail method, it used with the insertImage to
+     * populate the android.provider.MediaStore.Images.Media#insertImage with all the correct
+     * meta data. The StoreThumbnail method is private so it must be duplicated here.
+     *
+     * @see android.provider.MediaStore.Images.Media (StoreThumbnail private method)
+     */
+    private static Bitmap storeThumbnail(
+            ContentResolver cr,
+            Bitmap source,
+            long id,
+            float width,
+            float height,
+            int kind) {
+
+        // create the matrix to scale it
+        Matrix matrix = new Matrix();
+
+        float scaleX = width / source.getWidth();
+        float scaleY = height / source.getHeight();
+
+        matrix.setScale(scaleX, scaleY);
+
+        Bitmap thumb = Bitmap.createBitmap(source, 0, 0,
+                source.getWidth(),
+                source.getHeight(), matrix,
+                true
+        );
+
+        ContentValues values = new ContentValues(4);
+        values.put(Images.Thumbnails.KIND, kind);
+        values.put(Images.Thumbnails.IMAGE_ID, (int) id);
+        values.put(Images.Thumbnails.HEIGHT, thumb.getHeight());
+        values.put(Images.Thumbnails.WIDTH, thumb.getWidth());
+
+        Uri url = cr.insert(Images.Thumbnails.EXTERNAL_CONTENT_URI, values);
+
+        try {
+            OutputStream thumbOut = cr.openOutputStream(url);
+            thumb.compress(Bitmap.CompressFormat.JPEG, 100, thumbOut);
+            thumbOut.close();
+            return thumb;
+        } catch (FileNotFoundException ex) {
+            return null;
+        } catch (IOException ex) {
+            return null;
+        }
+    }
+}
